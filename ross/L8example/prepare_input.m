12b2497d06ff2b4021bb9454078ad33cbda64308
Ross Palomaki <ropa5718@core-gpu03.rc.int.colorado.edu>
2025-04-16 18:24:23 -0600

    streamline full tile processing

--- ross/L8example/prepare_input.m
+++ ross/L8example/prepare_input.m
@@ -1,6 +1,10 @@
 hdr = struct(); 
 
-point = [65.58711 -145.13650];
+pr = ;
+
+% point = [65.58711 -145.13650]; %p068r014 (Chena River basin AK)
+point = [39.318902, -107.137878]; %p035r033 (Grand Mesa CO)
+
 hdr.ProjectionStructure = createUTMmstruct(point);
 
 hdr.RasterReference = georasterinfo('DEM/tif/p068r014_dem.tif').RasterReference;
